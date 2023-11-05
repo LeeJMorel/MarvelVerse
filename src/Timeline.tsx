d23fe51cea426da0d3ec3fbe4f480d4749811605
Patrick Park <knu@Knu-Macbook-Pro.local>
2023-11-05 11:12:16 +0900

    5 Lanes version

--- src/Timeline.tsx
+++ src/Timeline.tsx
@@ -2,73 +2,34 @@ import React from "react";
 import "./App.scss";
 import { SelectedFilter } from "./types";
 import { Timeline as SVGTimeline } from "react-svg-timeline";
-// import marbleData from "./data/comics_filtered.json";
+import marbleData from "./data/marvel_comic_filtered.json";
 
 interface TimelineProps {
   filter: SelectedFilter;
 }
 
-const marbleData = [
-  {
-    comic_name: "Marvel Mystery Comics (1939 - 1949)",
-    active_years: "(1939 - 1949)",
-    issue_title: "Marvel Mystery Comics (1939) #2",
-    publish_date: "1-Dec-39",
-    issue_description:
-      "A historic comic series dating all the way back to 1939! Featuring the Human Torch, the Sub-Mariner and more, this one's sure to thrill any Marvel comic buff!",
-    penciler: "Al Anders, Carl Burgos, Bill Everett, Ben Thompson",
-    writer: "Ben Thompson, Paul Gustavson, Al Anders, Carl Burgos",
-    cover_artist: "None",
-    Imprint: "None",
-    Format: "Comic",
-    Rating: "None",
-    Price: "Free",
-  },
-  {
-    comic_name: "Marvel Mystery Comics (1939 - 1949)",
-    active_years: "(1939 - 1949)",
-    issue_title: "Marvel Mystery Comics (1939) #3",
-    publish_date: "1-Jan-40",
-    issue_description:
-      "A collection of epic stories featuring Human Torch in an alien invasion, Angel infiltrating a mind-bogglingly passionate cult and MUCH MORE!",
-    penciler: "Al Anders, Carl Burgos, Bill Everett, Ben Thompson",
-    writer: "Al Anders, Carl Burgos, Bill Everett, Ben Thompson",
-    cover_artist: "None",
-    Imprint: "None",
-    Format: "Comic",
-    Rating: "None",
-    Price: "Free",
-  },
-  {
-    comic_name: "Marvel Mystery Comics (1939 - 1949)",
-    active_years: "(1939 - 1949)",
-    issue_title: "Marvel Mystery Comics (1939) #4",
-    publish_date: "1-Feb-40",
-    issue_description:
-      "A collection of classics starring Marvel's now iconic Golden Age heroes! The Human Torch first adopts the name of Jim Hammond, and Namor the Sub-Mariner clashes with a Nazi U-boat for the first time!",
-    penciler:
-      "Stockbridge Winslow, Bill Everett, Steve Dahlman, Paul Gustavson",
-    writer: "Ben Thompson, Al Anders, Carl Burgos, Stockbridge Winslow",
-    cover_artist: "None",
-    Imprint: "None",
-    Format: "Comic",
-    Rating: "None",
-    Price: "Free",
-  },
-];
-
-const comicsEvents = marbleData.map((comic, index) => ({
-  eventId: comic.issue_title, // or some other unique identifier
+const comicsEvents = marbleData.map((comic) => ({
+  eventId: comic.issue_title,
   tooltip: comic.issue_description,
-  laneId: index,
+  laneId: comic.Grouping,
   startTimeMillis: parseDateToMillis(comic.publish_date),
-  endTimeMillis: parseDateToMillis(comic.publish_date) + 1000000000,
+  endTimeMillis: parseDateToMillis(comic.publish_date) + 100000000,
 }));
 
-const lanes = comicsEvents.map((comic) => ({
-  laneId: comic.laneId,
-  label: comic.eventId,
+const comicAbbreviationMap = {
+  AVE: "Avengers",
+  SPI: "Spiderman",
+  XM: "X-Men",
+  FF: "Fantastic Four",
+  MS: "Midnight Sons",
+  DEF: "Defenders",
+};
+
+const lanes = Object.entries(comicAbbreviationMap).map(([key, value]) => ({
+  laneId: key.toString(),
+  label: value,
 }));
+console.log(lanes);
 
 function parseDateToMillis(dateString) {
   const months = {
