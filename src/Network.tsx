0a11e265827a36eed9bc1fc3390dff5c3f2bb9a9
Lee Janzen-Morel <leejanzen91@gmail.com>
2023-11-10 10:25:55 -0800

    creates a fake social

--- src/Network.tsx
+++ src/Network.tsx
@@ -1,17 +1,25 @@
-import React from "react";
+import React, { useState } from "react";
 import "./App.scss";
-import { FilterOption } from "./types"; // Import the type
+import { FilterOption } from "./types";
+import SocialPost from "./SocialPost";
 
 interface NetworkProps {
   filter: FilterOption;
 }
 
-// eslint-disable-next-line @typescript-eslint/no-unused-vars
 const Network: React.FC<NetworkProps> = ({ filter }) => {
+  // eslint-disable-next-line @typescript-eslint/no-unused-vars
+  const [showSocialPost, setShowSocialPost] = useState(false);
   return (
-    <div className=".app-body.network-container">
-      <h2>Network Visualization</h2>
-      <p>This is a placeholder for the Network component.</p>
+    <div className="app-body network-container">
+      {showSocialPost && <SocialPost />}
+      <div className="network-graph-container">
+        <div className="title">
+          <h2>Network Visualization</h2>
+          <p>This is a placeholder for the Network component.</p>
+          <p>Current Filter: {filter.label}</p>
+        </div>
+      </div>
     </div>
   );
 };
