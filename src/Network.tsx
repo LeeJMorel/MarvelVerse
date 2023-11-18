1edc213517c93a56c636d20b0048345acf5b1993
Lee Janzen-Morel <leejanzen91@gmail.com>
2023-11-18 00:28:44 -0800

    creates a more complex network

--- src/Network.tsx
+++ src/Network.tsx
@@ -3,21 +3,12 @@ import "./App.scss";
 import { FilterOption } from "./types";
 import SocialPost from "./SocialPost";
 import { SocialPostProps } from "./types";
-import Graph from "graphology";
-import getNodeProgramImage from "sigma/rendering/webgl/programs/node.image";
-import { LayoutForceAtlas2Control } from "@react-sigma/layout-forceatlas2";
-import {
-  SigmaContainer,
-  ControlsContainer,
-  ZoomControl,
-  FullScreenControl,
-  SearchControl,
-} from "@react-sigma/core";
-import rawJson from "../src/data/temp2.json";
+import graphData from "../src/data/data.json";
 import "@react-sigma/core/lib/react-sigma.min.css";
+import { SigmaGraphViewer } from "./SigmaGraphViewer";
 
 // eslint-disable-next-line @typescript-eslint/no-explicit-any
-const jsonGraph: any = rawJson;
+const dataGraph: any = graphData;
 
 const fakeUsersData: Record<string, SocialPostProps> = {
   ironMan: {
@@ -57,7 +48,7 @@ interface NetworkProps {
 const Network: React.FC<NetworkProps> = ({ filter }) => {
   const [showSocialPost, setShowSocialPost] = useState(false);
   const [selectedUser, setSelectedUser] = useState<string | null>(null);
-  const graph = Graph.from(jsonGraph);
+
   const handleFollowingChange = (selectedUser: string): void => {
     const userData: SocialPostProps | undefined = fakeUsersData[selectedUser];
 
@@ -91,36 +82,12 @@ const Network: React.FC<NetworkProps> = ({ filter }) => {
 
       <div className="network-graph-container">
         <div className="title">
-          <h2>Network Visualization</h2>
-          <p>This is a placeholder for the Network component.</p>
-          <SigmaContainer
-            graph={graph}
-            style={{ height: "500px" }}
-            settings={{
-              nodeProgramClasses: { image: getNodeProgramImage() },
-              defaultNodeType: "image",
-              defaultEdgeType: "arrow",
-              labelDensity: 0.07,
-              labelGridCellSize: 60,
-              labelRenderedSizeThreshold: 15,
-              labelFont: "Lato, sans-serif",
-              zIndex: true,
-            }}
-          >
-            <ControlsContainer position={"bottom-right"}>
-              <ZoomControl />
-              <FullScreenControl />
-              <LayoutForceAtlas2Control />
-            </ControlsContainer>
-            <ControlsContainer position={"top-right"}>
-              <SearchControl style={{ width: "200px" }} />
-            </ControlsContainer>
-          </SigmaContainer>
           <p>Current Filter: {filter.label}</p>
-          {showSocialPost && (
-            <button onClick={handleTestButtonClick}>Test Button</button>
-          )}
         </div>
+        <SigmaGraphViewer data={dataGraph} />
+        {showSocialPost && (
+          <button onClick={handleTestButtonClick}>Test Button</button>
+        )}
       </div>
     </div>
   );
---
+++ src/SigmaGraphViewer.tsx
@@ -0,0 +1,152 @@
+/* eslint-disable @typescript-eslint/no-explicit-any */
+import React, { FC, useEffect, useState } from "react";
+import {
+  ControlsContainer,
+  FullScreenControl,
+  SearchControl,
+  SigmaContainer,
+  ZoomControl,
+  useLoadGraph,
+  useRegisterEvents,
+  useSetSettings,
+  useSigma,
+} from "@react-sigma/core";
+import {
+  AiOutlineZoomIn,
+  AiOutlineZoomOut,
+  AiOutlineFullscreenExit,
+  AiOutlineFullscreen,
+} from "react-icons/ai";
+import { MdFilterCenterFocus } from "react-icons/md";
+import { MultiDirectedGraph } from "graphology";
+import "./App.scss";
+import { Attributes } from "graphology-types";
+
+interface Node {
+  label: string;
+  x: number;
+  y: number;
+  id: string;
+  attributes: Record<string, any>;
+  color: string;
+  size: number;
+}
+
+interface Edge {
+  source: string;
+  target: string;
+  id: string;
+  attributes: Record<string, any>;
+  color: string;
+  size: number;
+}
+
+interface GraphData {
+  nodes: Node[];
+  edges: Edge[];
+}
+
+interface SigmaGraphViewerProps {
+  data: GraphData;
+}
+
+const MyGraph: FC<{ data: GraphData }> = ({ data }) => {
+  const loadGraph = useLoadGraph();
+  const sigma = useSigma();
+  const registerEvents = useRegisterEvents();
+  const setSettings = useSetSettings();
+  const [hoveredNode, setHoveredNode] = useState<string | null>(null);
+
+  useEffect(() => {
+    // Create the graph
+    const graph = new MultiDirectedGraph();
+
+    data.nodes.forEach((node, index) => {
+      graph.addNode(String(index), { ...node });
+    });
+
+    data.edges.forEach((edge, index) => {
+      graph.addEdgeWithKey(String(index), edge.source, edge.target, {
+        color: edge.color,
+      });
+    });
+
+    loadGraph(graph);
+
+    registerEvents({
+      enterNode: (event) => setHoveredNode(event.node),
+      leaveNode: () => setHoveredNode(null),
+    });
+  }, [data, loadGraph, registerEvents]);
+
+  useEffect(() => {
+    setSettings({
+      nodeReducer: (node, data) => {
+        const graph = sigma.getGraph();
+        const newData: Attributes = {
+          ...data,
+          highlighted: data.highlighted || false,
+        };
+
+        if (hoveredNode) {
+          if (
+            node === hoveredNode ||
+            graph.neighbors(hoveredNode).includes(node)
+          ) {
+            newData.highlighted = true;
+          } else {
+            newData.color = "#E2E2E2";
+            newData.highlighted = false;
+          }
+        }
+        return newData;
+      },
+      edgeReducer: (edge, data) => {
+        const graph = sigma.getGraph();
+        const newData = { ...data, hidden: false };
+
+        if (hoveredNode && !graph.extremities(edge).includes(hoveredNode)) {
+          newData.hidden = true;
+        }
+        return newData;
+      },
+    });
+  }, [hoveredNode, setSettings, sigma]);
+
+  return null;
+};
+
+export const SigmaGraphViewer: FC<SigmaGraphViewerProps> = ({ data }) => {
+  return (
+    <SigmaContainer
+      style={{
+        width: "100%",
+        height: "100%",
+        margin: "auto",
+        overflow: "hidden",
+        background: "transparent",
+      }}
+      settings={{
+        labelFont: "Lato, sans-serif",
+        zIndex: true,
+        defaultEdgeType: "arrow",
+      }}
+    >
+      <ControlsContainer position="top-left">
+        <ZoomControl>
+          <AiOutlineZoomIn color="black" />
+          <AiOutlineZoomOut color="black" />
+          <MdFilterCenterFocus color="black" />
+        </ZoomControl>
+        <FullScreenControl>
+          <AiOutlineFullscreen color="black" />
+          <AiOutlineFullscreenExit color="black" />
+        </FullScreenControl>
+      </ControlsContainer>
+      <ControlsContainer position={"top-right"}>
+        <SearchControl style={{ width: "200px" }} />
+      </ControlsContainer>
+      <MyGraph data={data} />
+    </SigmaContainer>
+  );
+};
