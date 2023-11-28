5f41daff08a8c55d4168240c45d111252a644c72
C Bendix <christopherbendix@gmail.com>
2023-11-27 16:37:11 -0800

    minor changes to team bios

--- src/HowToPost.tsx
+++ src/HowToPost.tsx
@@ -36,16 +36,16 @@ const HowToPost: React.FC<HowToPostPropsWithCallback> = ({
         "To learn more about the major hero teams in Marvel, click a filter in the bottom navigation. If you click to get a closer look at these teams, you can also double-click any of the characters in the teams to learn a bit more about them and who they know in the Marvel social network. We recommend looking at the timeline as well to see more history about the teams.",
     },
     avengers: {
-      title: "Avengers",
+      title: "The Avengers",
       subheader: "Assembling Heroes Since 1963",
       textBody:
         "The Avengers, formed in 1963, have been a symbol of unity and strength. Originally composed of Iron Man, Thor, Hulk, Ant-Man, and Wasp, the team has grown to include many iconic Marvel characters over the years. Known for their epic battles against powerful foes, the Avengers stand as Earth's mightiest heroes.",
     },
     spiderman: {
-      title: "Spiderman",
+      title: "Spider-Man",
       subheader: "Swinging Since 1962",
       textBody:
-        "Spiderman, created in 1962, has been swinging through the streets of New York for decades. Peter Parker, a high school student bitten by a radioactive spider, balances the challenges of life and crime-fighting. With his iconic red and blue suit, Spiderman has become one of Marvel's most beloved characters.",
+        "Spider-Man, created in 1962, has been swinging through the streets of New York for decades. Peter Parker, a high school student bitten by a radioactive spider, balances the challenges of life and crime-fighting. With his iconic red and blue suit, Spider-Man has become one of Marvel's most beloved characters.",
     },
     xmen: {
       title: "X-Men",
@@ -55,7 +55,7 @@ const HowToPost: React.FC<HowToPostPropsWithCallback> = ({
     },
     fantasticfour: {
       title: "Fantastic Four",
-      subheader: "Adventuring Since 1961",
+      subheader: "The World's Greatest Since 1961",
       textBody:
         "The Fantastic Four, debuting in 1961, consists of Mr. Fantastic, Invisible Woman, Human Torch, and The Thing. Endowed with unique powers after a space mission gone wrong, they explore the unknown and face cosmic threats. The Fantastic Four is Marvel's first superhero team.",
     },
@@ -66,7 +66,7 @@ const HowToPost: React.FC<HowToPostPropsWithCallback> = ({
         "The Midnight Sons, formed in 1992, confront supernatural and mystical threats. Comprising characters like Ghost Rider, Blade, and Morbius, they battle dark forces that lurk in the shadows. The Midnight Sons bring a supernatural edge to the Marvel Universe.",
     },
     defenders: {
-      title: "Defenders",
+      title: "The Defenders",
       subheader: "Protecting Reality Since 1971",
       textBody:
         "The Defenders, established in 1971, are a unique team brought together by mystical and cosmic forces. With ever-changing members, including Doctor Strange, Hulk, and Silver Surfer, the Defenders protect reality from magical and cosmic threats. Their stories often delve into the supernatural and cosmic aspects of the Marvel Universe.",
